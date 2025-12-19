38d6f7fecd4606d1a5b43a3d1425d88926d69ab1
Marc <marc.perez.dev@gmail.com>
2025-12-19 00:11:01 -0800

    action tile icons

--- ui/scripts/action_tile.gd
+++ ui/scripts/action_tile.gd
@@ -2,13 +2,22 @@
 class_name ActionTile extends PanelContainer
 
 @onready var text_label: Label = %TextLabel
+@onready var icon_texture: TextureRect = %IconTexture
 
 @export var action: Car.Action = Car.Action.ACCELERATE:
 	set(value):
 		action = value
-		if text_label:
+		if text_label and icon_texture:
 			text_label.text = Car.Action.keys()[action].replace("_", " ")
-
+			match action:
+				Car.Action.ACCELERATE:
+					icon_texture.texture.region.position.y = 256.0
+				Car.Action.DECELERATE:
+					icon_texture.texture.region.position.y = 192.0
+				Car.Action.TURN_LEFT:
+					icon_texture.texture.region.position.y = 128.0
+				Car.Action.TURN_RIGHT:
+					icon_texture.texture.region.position.y = 64.0
 
 # ENGINE
 func _ready():
